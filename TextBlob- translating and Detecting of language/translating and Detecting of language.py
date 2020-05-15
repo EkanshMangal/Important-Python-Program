040111c4bebfeb75833ac50d2fb8d7f985f63f5f
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-05-16 00:26:58 +0530

    TextBlob

--- TextBlob- translating and Detecting of language/translating and Detecting of language.py
+++ TextBlob- translating and Detecting of language/translating and Detecting of language.py
@@ -1,7 +1,11 @@
 # - *- coding: utf- 8 - *-
 from textblob import TextBlob
-word1=TextBlob("thank you everyone for coming")
+word1=TextBlob("thank you for using this")
 lang=word1.detect_language()
 z=word1.translate(from_lang='en', to ='hi')
 print(lang)
 print(z)
+
+# first install textblob library for using this
+# pip install textblob
+# encoding format should be exact and placed at beginning of file

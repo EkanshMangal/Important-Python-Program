64d0a143374c3fdcdb29d61e4bce132f1c380c07
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-05-23 00:53:26 +0530

    detection added

--- Googletrans- translating and Detcting language/translating and detetcting.py
+++ Googletrans- translating and Detcting language/translating and detetcting.py
@@ -1,23 +1,27 @@
 # 1) Translate
 from googletrans import Translator
 trans = Translator()
+# to detect language
+txt="Hello How are you"
+dial_lang=trans.detect(txt).lang
+print(dial_lang)
 t = trans.translate(
-    'tum idhar mat aao', src= 'hi', dest='hi'
+    txt, src= dial_lang, dest='hi'
 )
 print(f'Source: {t.src}')
 print(f'Destination: {t.dest}')
 print(f'{t.origin} -> {t.text}')
-print()
-# # 2) List supported languages
-# from googletrans import LANGUAGES
-
-# for lang in LANGUAGES:
-#     print(f'{lang} - {LANGUAGES[lang]}')
 # print()
+# # # 2) List supported languages
+# # from googletrans import LANGUAGES
 
-# # 3) List possible mistakes and translations 
-pm = t.extra_data['possible-mistakes']
-pt = t.extra_data['possible-translations']
+# # for lang in LANGUAGES:
+# #     print(f'{lang} - {LANGUAGES[lang]}')
+# # print()
 
-print(f'Possible Mistakes: {pm}')
-print(f'Possible Translations: {pt}')
+# # # 3) List possible mistakes and translations 
+# pm = t.extra_data['possible-mistakes']
+# pt = t.extra_data['possible-translations']
+
+# print(f'Possible Mistakes: {pm}')
+# print(f'Possible Translations: {pt}')

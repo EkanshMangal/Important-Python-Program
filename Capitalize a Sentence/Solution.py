3c9c5602106a7f9579c7c4a744b21c0286895c7f
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-06-08 14:57:10 +0530

    File added

---
+++ Capitalize a Sentence/Solution.py
@@ -0,0 +1,10 @@
+string = "first letter should be capital"
+capitalized_string = string.capitalize()
+print('Original String: ', string)
+print('Updated String: ', capitalized_string)
+
+
+#output
+#Original String:  first letter should be capital
+#Updated String:  First letter should be capital
+
